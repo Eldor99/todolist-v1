a5c17966b32d5ddbe2fb51d692fe7911ca4064be
Eldor99 <eldor1999@protonmail.com>
2020-11-14 13:35:08 +0500

    added ejs template

--- app.js
+++ app.js
@@ -2,21 +2,49 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
 
 const app = express();
 const port = 3000;
 
 
+app.set('view engine','ejs');
+
 app.get('/', (req,res) =>{
 
 	let today = new Date();
 	let currentDay = today.getDay();
+	let day = '';
 
-	if(currentDay === 6 || currentDay === 0){
-		res.send('<h1>Yay it is the weekend</h1>');
-	}else{
-		res.sendFile(__dirname + '/index.html');
+	switch (currentDay) {
+		case 0:
+			day = 'Sunday';
+			break;
+		case 1:
+			day = 'Monday';
+			break;
+		case 2:
+			day = 'Tuesday';
+			break;
+		case 3:
+			day = 'Wednesday';
+			break;
+		case 4:
+			day = 'Thursday';
+			break;
+		case 5:
+			day = 'Friday';
+			break;
+		case 6:
+			day = 'Saturday ';
+			break;
+		default:
+			day = '';
+			break;
 	}
+
+		res.render('list', {kindOfDay: day});
+
 });
 
 app.listen(port, ()=>{
