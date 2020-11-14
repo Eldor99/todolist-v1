f895844f2dac7030cb245488a2d5531a186ad8e1
Eldor99 <eldor1999@protonmail.com>
2020-11-14 18:03:11 +0500

    added module

--- app.js
+++ app.js
@@ -3,11 +3,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const date = require(__dirname + '/date.js');
 
 const app = express();
 const port = 3000;
-let items= [];
-let workItems = [];
+const items= [];
+const workItems = [];
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
@@ -16,15 +17,7 @@ app.set('view engine','ejs');
 
 app.get('/', (req,res) =>{
 
-	let today = new Date();
-
-	let options = {
-		weekday: 'long',
-		day: 'numeric',
-		month: 'long'
-	};
-
-	let day = today.toLocaleDateString('en-US', options);
+	const day = date();
 
 	res.render('list', {listTitle: day, newListItem: items});
 
@@ -40,7 +33,7 @@ app.get('/about', (req,res) => {
 });
 
 app.post('/', (req,res)=>{
-	let item = req.body.newItem;
+	const item = req.body.newItem;
 	if(req.body.list === 'Work'){
 		workItems.push(item);
 		res.redirect('/work');
@@ -51,7 +44,7 @@ app.post('/', (req,res)=>{
 })
 
 app.post('/work',(req,res)=>{
-	let item = req.body.newItem;
+	const item = req.body.newItem;
 	workItems.push(item);
 	res.redirect('/work');
 });
