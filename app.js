e8e631a1e01295fda484cc11b3a5abbe51e5f5cc
Eldor99 <eldor1999@protonmail.com>
2020-11-14 14:38:37 +0500

    added list

--- app.js
+++ app.js
@@ -6,46 +6,36 @@ const ejs = require('ejs');
 
 const app = express();
 const port = 3000;
+var items= [];
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine','ejs');
 
 app.get('/', (req,res) =>{
 
 	let today = new Date();
-	let currentDay = today.getDay();
-	let day = '';
 
-	switch (currentDay) {
-		case 0:
-			day = 'Sunday';
-			break;
-		case 1:
-			day = 'Monday';
-			break;
-		case 2:
-			day = 'Tuesday';
-			break;
-		case 3:
-			day = 'Wednesday';
-			break;
-		case 4:
-			day = 'Thursday';
-			break;
-		case 5:
-			day = 'Friday';
-			break;
-		case 6:
-			day = 'Saturday ';
-			break;
-		default:
-			day = '';
-			break;
-	}
+	let options = {
+		weekday: 'long',
+		day: 'numeric',
+		month: 'long'
+	};
 
-		res.render('list', {kindOfDay: day});
+	let day = today.toLocaleDateString('en-US', options);
+
+	res.render('list', {kindOfDay: day, newListItem: items});
 
 });
+
+
+app.post('/', (req,res)=>{
+	var item = req.body.newItem;
+
+	items.push(item);
+
+	res.redirect('/');
+})
 
 app.listen(port, ()=>{
 	console.log('Server started on port ' + port);
